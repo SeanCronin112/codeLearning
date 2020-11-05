aff302793b0b5b1d8221a9073f9a6be9a6efe6c3
Sean Cronin <cpcsean@gmail.com>
2020-11-05 12:02:47 +0000

    Adding the ReadMe file and the Arrays file.

---
+++ c/README.txt
@@ -0,0 +1,2 @@
+My sample code that I am keeping for future reference. 
+All this code is based off the freeCodeCamp C tutorial, which can be found at https://www.youtube.com/watch?v=KJgsSFOSQv0
---
+++ c/arrays.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main()
+{
+    /* Defined Lists
+    int luckyNumbers[] = {1, 2, 3, 4, 5, 6};
+    luckyNumbers[1] = 200;
+    printf("%d", luckyNumbers[1]);
+    */
+
+   /* Undefined Lists
+   int unluckyNumbers[10];
+   unluckyNumbers[1] = 200;
+   printf("%d", unluckyNumbers[1]);
+   */
+}
